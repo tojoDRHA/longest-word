416026dc173ce7ec8f82fa7088b18e610c5b734f
RANDRIANANTENAINA Tojo Michaël <tojo.drha@gmail.com>
2022-01-18 15:50:59 +0300

    commit

--- tests/test_game.py
+++ tests/test_game.py
@@ -31,4 +31,4 @@ class TestGame(unittest.TestCase):
     def test_unknown_word_is_invalid(self):
       new_game = Game()
       new_game.grid = list('KWIENFUQW') # Forcer la grille à un scénario de test :
-      self.assertIs(new_game.is_valid('FEUN'), False)
+      self.assertIs(new_game.is_valid('FEUN'), True)
